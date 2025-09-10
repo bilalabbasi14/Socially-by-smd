fc337d45d9513ba2e66bba72ffbade965ffb51cf
bilalabbasi14 <bilalabbasi1404@gmail.com>
2025-09-11 02:03:37 +0500

    Socially app UI completed till dms

--- app/src/main/java/com/example/sociallybysmd/dm.kt
+++ app/src/main/java/com/example/sociallybysmd/dm.kt
@@ -3,25 +3,28 @@ package com.example.sociallybysmd
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class feed : AppCompatActivity() {
+class dm : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_feed)
+        setContentView(R.layout.activity_dm)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val dm= findViewById<ImageView>(R.id.dms)
-        dm.setOnClickListener {
-            val dmsIntent= Intent(this, dm::class.java)
-            startActivity(dmsIntent)
+        val dmsBack=findViewById<ImageView>(R.id.dmback)
+        dmsBack.setOnClickListener {
+            val dmBackIntent= Intent(this, feed::class.java)
+            startActivity(dmBackIntent)
         }
+
+
     }
 }
--- app/src/main/java/com/example/sociallybysmd/opendm.kt
+++ app/src/main/java/com/example/sociallybysmd/opendm.kt
@@ -1,27 +1,20 @@
 package com.example.sociallybysmd
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class feed : AppCompatActivity() {
+class opendm : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_feed)
+        setContentView(R.layout.activity_opendm)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        val dm= findViewById<ImageView>(R.id.dms)
-        dm.setOnClickListener {
-            val dmsIntent= Intent(this, dm::class.java)
-            startActivity(dmsIntent)
         }
     }
 }
